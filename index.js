907e88a91df067cf5750c082bd8ab0d51932ab28
nikusha1446 <nikusha1446@gmail.com>
2025-09-22 20:01:03 +0400

    feat: add movie formatting functionality

--- index.js
+++ index.js
@@ -17,8 +17,34 @@ const fetchMoviesData = async (type) => {
   const url = `https://api.themoviedb.org/3/movie/${type}?language=en-US&page=1&api_key=${apiKey}`;
   try {
     const response = await axios.get(url);
-    console.log(response);
+    formatMovies(response.data);
   } catch (error) {
     console.error(error);
   }
+};
+
+const formatMovies = (data) => {
+  const movies = data.results;
+
+  console.log('');
+  console.log(`${movies.length} Movies Found`);
+  console.log('');
+
+  movies.forEach((movie, index) => {
+    const rating = movie.vote_average.toFixed(1);
+    const year = new Date(movie.release_date).getFullYear();
+    const popularity = Math.round(movie.popularity);
+
+    const movieNumber = `${index + 1}.`;
+    const indent = movieNumber.length + 1;
+
+    console.log(`${movieNumber} ${movie.title}`);
+    console.log(
+      `${''.padStart(
+        indent
+      )}Year: ${year} | ⭐ ${rating}/10 | 🔥 ${popularity} popularity`
+    );
+    console.log(`${''.padStart(indent)}${movie.overview.substring(0, 90)}...`);
+    console.log('');
+  });
 };
